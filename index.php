215f15b8717df0c2003a1e98def6b1e7243c2d01
insight tech <119024802+insight-tech2@users.noreply.github.com>
2023-01-16 12:28:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <title></title>
+  <link rel="stylesheet" href="./style.css">
+<script>
+        var timer = setTimeout(function() {
+            window.location='/love.php'
+        }, 20000);
+</script>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<html>
+<head>
+  <title>love you</title>
+</head>
+<body>
+<div class="bg_heart">
+</div>
+
+<div class="container">
+<h2 id="text1"></h1>
+</html>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script><script  src="./script.js"></script>
+</footer>
+</body>
+</html>
